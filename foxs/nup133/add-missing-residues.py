e0b7f61e5d92898673f410d8b7269faed34e8793
Ben Webb <ben@salilab.org>
2019-04-12 16:21:53 -0700

    Simplify Modeller script

--- foxs/nup133/add-missing-residues.py
+++ foxs/nup133/add-missing-residues.py
@@ -10,14 +10,6 @@ from modeller.automodel import *    # Load the automodel class
 log.verbose()    # request verbose output
 env = environ()  # create a new MODELLER environment to build this model in
 
-# script_dir is the directory this script is in
-import sys
-import os
-script_dir = os.path.dirname(sys.argv[0])
-
-# directories for input atom files
-env.io.atom_files_directory = [script_dir, '../atom_files']
-
 # Renumber output model to get correct residue numbers and chain IDs
 # (Modeller default is to not assign a chain ID, and start residue numbers at 1)
 class MyModel(automodel):
@@ -25,8 +17,7 @@ class MyModel(automodel):
         self.rename_segments(segment_ids='A', renumber_residues=944)
 
 a = MyModel(env,
-            alnfile  = os.path.join(script_dir,
-                                    'alignment.ali'),   # alignment filename
+            alnfile  = 'alignment.ali',     # alignment filename
             knowns   = '3KFO',              # codes of the templates
             sequence = '3KFO-fill')         # code of the target
 a.starting_model= 1                 # index of the first model
