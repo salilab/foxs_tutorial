bdf8da99e548be55fe3eab3a889c0972f0733222
Ben Webb <ben@salilab.org>
2023-12-03 16:01:05 -0800

    Use older pip to fix Python 2 CI

--- support/setup_ci.sh
+++ support/setup_ci.sh
@@ -13,13 +13,20 @@ python_version=$2
 # get conda-forge, not main, packages
 conda config --remove channels defaults
 conda config --add channels conda-forge
+
+if [ ${python_version} = "2.7" ]; then
+  pip="pip<=19.3.1"
+else
+  pip="pip"
+fi
+
 if [ ${imp_branch} = "develop" ]; then
   IMP_CONDA="imp-nightly"
 else
   IMP_CONDA="imp"
 fi
 
-conda create --yes -q -n python${python_version} -c salilab python=${python_version} modeller ${IMP_CONDA}
+conda create --yes -q -n python${python_version} -c salilab python=${python_version} modeller ${pip} ${IMP_CONDA}
 eval "$(conda shell.bash hook)"
 conda activate python${python_version}
 
